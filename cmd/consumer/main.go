0db55edd50d47ceff5a2d8274723d2f10b409de9
Dylan Carney <dylan.carney@hbo.com>
2018-07-24 16:02:31 -0700

    Adds file-based logging to the sample record processor

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"math/big"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/Clever/amazon-kinesis-client-go/kcl"
@@ -14,16 +18,32 @@ type sampleRecordProcessor struct {
 	checkpointFreq time.Duration
 	largestPair    kcl.SequencePair
 	lastCheckpoint time.Time
+	log            io.Writer
 }
 
 func newSampleRecordProcessor() *sampleRecordProcessor {
 	return &sampleRecordProcessor{
+		log:            ioutil.Discard,
 		checkpointFreq: 60 * time.Second,
 	}
 }
 
 func (srp *sampleRecordProcessor) Initialize(shardID string, checkpointer kcl.Checkpointer) error {
+	// Use a file for all the logging, so that stdout remains dedicated to communication between this
+	// process and the MLD.  The name of the file is unique to the PID and the shard ID being processed,
+	// so that multiple processes started by the same MLD don't conflict.
+	//
+	// TODO: this obviously shouldn't be a hardcoded path, but it's enough for a prototype
+	f, err := os.Create(filepath.Join("/tmp/kcl_stderr", fmt.Sprintf("%s-%d", shardID, os.Getpid())))
+	if err != nil {
+		panic(err)
+	}
+	srp.log = f
+
+	// logging statements for debugging of the prototype
+	fmt.Fprintf(srp.log, "initializing with shardID: %q and checkpointer: %#v\n", shardID, checkpointer)
 	srp.lastCheckpoint = time.Now()
+	fmt.Fprintf(srp.log, "last checkpoint: %s", srp.lastCheckpoint.Format(time.RFC3339))
 	srp.checkpointer = checkpointer
 	return nil
 }
@@ -58,15 +78,18 @@ func (srp *sampleRecordProcessor) Shutdown(reason string) error {
 	} else {
 		fmt.Fprintf(os.Stderr, "Shutting down due to failover. Will not checkpoint.\n")
 	}
+
+	if srp.log != nil {
+		if f, ok := srp.log.(*os.File); ok {
+			fmt.Fprintf(srp.log, "Closing log file\n")
+			f.Close()
+		}
+	}
+
 	return nil
 }
 
 func main() {
-	f, err := os.Create("/tmp/kcl_stderr")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
 	kclProcess := kcl.New(os.Stdin, os.Stdout, os.Stderr, newSampleRecordProcessor())
 	kclProcess.Run()
 }
